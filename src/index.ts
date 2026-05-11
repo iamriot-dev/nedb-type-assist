3018bae78e8dfb9f24a35aa15b5be5a13f93ca28
iamriot-dev <59467725+iamriot-dev@users.noreply.github.com>
2026-05-12 02:14:57 +0800

    fix $set for upsert

--- src/index.ts
+++ src/index.ts
@@ -78,60 +78,64 @@ type $Update<
 	Upsert extends boolean | undefined,
 > =
 	| Omit<T, "_id">
-	| RequireAtLeastOne<
-			(Upsert extends true
-				? { $set: Omit<T, "_id"> }
-				: { $set?: { [Key in Paths<T>]?: NonNullableDeep<Get<T, Key>> } }) & {
-				$unset?: {
-					[Key in Paths<T> as IsOptional<Get<T, Key>> extends true
-						? Key
-						: never]?: true;
-				};
-				$push?: {
-					[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
-						? Key
-						: never]?: NonNullable<Get<T, Key>> extends Array<infer A>
-						? A | RequireAtLeastOne<{ $each?: A[]; $slice?: number }>
-						: never;
-				};
-				$pull?: {
-					[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
-						? Key
-						: never]?: NonNullable<Get<T, Key>> extends Array<infer A>
-						? (Pick<$ArrayOp<A, never>, "$elemMatch"> | { $in: A[] }) | A
-						: never;
-				};
-				$pop?: 1 | -1;
-				$addToSet?: {
-					[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
-						? Key
-						: never]?: NonNullable<Get<T, Key>> extends Array<infer A>
-						? A | RequireAtLeastOne<{ $each?: A[]; $slice?: number }>
-						: never;
-				};
-				$inc?: {
-					[Key in Paths<T> as NonNullable<Get<T, Key>> extends string
-						? Key
-						: never]?: NonNullable<Get<T, Key>>;
-				};
-				$min?: {
-					[Key in Paths<T> as NonNullable<Get<T, Key>> extends
-						| string
-						| number
-						| Date
-						? Key
-						: never]?: NonNullable<Get<T, Key>>;
-				};
-				$max?: {
-					[Key in Paths<T> as NonNullable<Get<T, Key>> extends
-						| string
-						| number
-						| Date
-						? Key
-						: never]?: NonNullable<Get<T, Key>>;
-				};
-			}
-	  >;
+	| (Upsert extends true
+			? { $set: Omit<T, "_id"> } & $UpdateBaseOps<T>
+			: RequireAtLeastOne<
+					{
+						$set?: { [Key in Paths<T>]?: NonNullableDeep<Get<T, Key>> };
+					} & $UpdateBaseOps<T>
+				>);
+
+type $UpdateBaseOps<T extends Record<string, unknown>> = {
+	$unset?: {
+		[Key in Paths<T> as IsOptional<Get<T, Key>> extends true
+			? Key
+			: never]?: true;
+	};
+	$push?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
+			? Key
+			: never]?: NonNullable<Get<T, Key>> extends Array<infer A>
+			? A | RequireAtLeastOne<{ $each?: A[]; $slice?: number }>
+			: never;
+	};
+	$pull?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
+			? Key
+			: never]?: NonNullable<Get<T, Key>> extends Array<infer A>
+			? (Pick<$ArrayOp<A, never>, "$elemMatch"> | { $in: A[] }) | A
+			: never;
+	};
+	$pop?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
+			? Key
+			: never]?: NonNullable<Get<T, Key>> extends Array<infer _>
+			? 1 | -1
+			: never;
+	};
+	$addToSet?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends Array<infer _>
+			? Key
+			: never]?: NonNullable<Get<T, Key>> extends Array<infer A>
+			? A | RequireAtLeastOne<{ $each?: A[]; $slice?: number }>
+			: never;
+	};
+	$inc?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends number
+			? Key
+			: never]?: NonNullable<Get<T, Key>>;
+	};
+	$min?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends string | number | Date
+			? Key
+			: never]?: NonNullable<Get<T, Key>>;
+	};
+	$max?: {
+		[Key in Paths<T> as NonNullable<Get<T, Key>> extends string | number | Date
+			? Key
+			: never]?: NonNullable<Get<T, Key>>;
+	};
+};
 
 type WrapOptions =
 	| {
